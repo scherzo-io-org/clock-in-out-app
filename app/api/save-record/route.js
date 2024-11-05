2e537a1e4b0fe6dfbafb64cee12b4511d8571778
Andriy <andriy.etcheverry@gmail.com>
2024-11-05 15:37:21 +0000

    change image val in db to just filename

--- app/api/save-record/route.js
+++ app/api/save-record/route.js
@@ -4,13 +4,15 @@ import { getDBConnection, sql } from '@/lib/db';
 export async function POST(request) {
   const { id, action, imageUrl } = await request.json();
 
+  const filename = new URLSearchParams(imageUrl.split('?')[1]).get('filename');
+
   try {
     const pool = await getDBConnection();
     const result = await pool
       .request()
       .input('id_num', sql.VarChar, id)
       .input('type', sql.VarChar, action)
-      .input('image_url', sql.NVarChar, imageUrl)
+      .input('image_url', sql.NVarChar, filename)
       .query(`INSERT INTO clockinsouts (REP, type, timestamp_in, image_url)
       VALUES (@id_num, @type, GETDATE(), @image_url)`);
 
